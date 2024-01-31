7c81c82b436be5c201cbabc2745c0cc4d67ef74b
Aditya Jain <93572760+aditya8462@users.noreply.github.com>
2024-01-31 11:30:48 +0530

    Update FetchServices.js

--- src/ServerServices/FetchServices.js
+++ src/ServerServices/FetchServices.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const ServerURL = "http://localhost:5000";
+const ServerURL = "http://campusshala.com:3236";
 const getData = async (url) => {
   try {
     var response = await fetch(`${ServerURL}/${url}`);
